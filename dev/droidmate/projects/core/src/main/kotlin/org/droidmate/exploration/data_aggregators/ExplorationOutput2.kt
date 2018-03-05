24e9c9706ac01f96c36348247cbb594ccd50e6a8
natanieljr <Nataniel.123>
2018-03-05 16:49:37 +0100

    Renamed class and interface (I)ApkExplorationOutput2 to (I)ExplorationLog

--- dev/droidmate/projects/core/src/main/kotlin/org/droidmate/exploration/data_aggregators/ExplorationOutput2.kt
+++ dev/droidmate/projects/core/src/main/kotlin/org/droidmate/exploration/data_aggregators/ExplorationOutput2.kt
@@ -21,15 +21,15 @@ package org.droidmate.exploration.data_aggregators
 import org.droidmate.storage.IStorage2
 import org.slf4j.LoggerFactory
 
-class ExplorationOutput2(private val list: MutableList<IApkExplorationOutput2> = ArrayList()) : MutableList<IApkExplorationOutput2> by list {
+class ExplorationOutput2(private val list: MutableList<IExplorationLog> = ArrayList()) : MutableList<IExplorationLog> by list {
     companion object {
         private val log = LoggerFactory.getLogger(ExplorationOutput2::class.java)
         private const val serialVersionUID: Long = 1
 
         @JvmStatic
-        fun from(storage: IStorage2): List<IApkExplorationOutput2> {
+        fun from(storage: IStorage2): List<IExplorationLog> {
             return storage.getSerializedRuns2().map {
-                val apkout2 = storage.deserialize(it) as IApkExplorationOutput2
+                val apkout2 = storage.deserialize(it) as IExplorationLog
                 log.info("Deserialized exploration output of $apkout2.packageName from $it")
                 apkout2.verify()
                 apkout2
