8799d142e3e0edd5b65575992b23700830931838
Timo Gühring <timo.guehring@googlemail.com>
2018-07-09 17:02:29 +0200

    Include soot as jar dependency and enable the CoverageCommand

--- project/pcComponents/command/src/main/kotlin/org/droidmate/command/CoverageCommand.kt
+++ project/pcComponents/command/src/main/kotlin/org/droidmate/command/CoverageCommand.kt
@@ -1,4 +1,29 @@
-/*package org.droidmate.command
+// DroidMate, an automated execution generator for Android apps.
+// Copyright (C) 2012-2018. Saarland University
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Current Maintainers:
+// Nataniel Borges Jr. <nataniel dot borges at cispa dot saarland>
+// Jenny Hotzkow <jenny dot hotzkow at cispa dot saarland>
+//
+// Former Maintainers:
+// Konrad Jamrozik <jamrozik at st dot cs dot uni-saarland dot de>
+//
+// web: www.droidmate.org
+
+package org.droidmate.command
 
 import com.konradjamrozik.createDirIfNotExists
 import org.droidmate.androcov.StatementInstrumenter
@@ -33,4 +58,4 @@ class CoverageCommand @JvmOverloads constructor(cfg: ConfigurationWrapper,
 
 		return emptyList()
 	}
-}*/
+}
