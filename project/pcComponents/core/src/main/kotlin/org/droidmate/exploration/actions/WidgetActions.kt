c4fd30f424efd82a0e9661fa2d7106315da53a8b
J.H <hotzkow@cs.uni-saarland.de>
2018-07-30 17:56:28 +0200

    added ignore visibility option for widget actions

--- project/pcComponents/core/src/main/kotlin/org/droidmate/exploration/actions/WidgetActions.kt
+++ project/pcComponents/core/src/main/kotlin/org/droidmate/exploration/actions/WidgetActions.kt
@@ -19,26 +19,28 @@ import org.droidmate.exploration.statemodel.Widget
  * The whidget has to be clickable and enabled. If it is not visible this method will throw an exception
  * (you should use [navigateTo] instead).
  */
-fun Widget.click(delay:Long = 0): ExplorationAction{
-	if(!visible || !enabled || !clickable)
+@JvmOverloads fun Widget.click(delay:Long = 0, isVisible: Boolean=false): ExplorationAction{
+	if(!(visible||isVisible) || !enabled || !clickable)
 		throw RuntimeException("ERROR: tried to click non-actable Widget $this")
 	widgetTargets.add(this)
 	return clickCoordinate().let{ (x,y) -> Click(x, y,true,delay) }
 }
-fun Widget.tick(): ExplorationAction{
-	if(!visible || !enabled)
+@JvmOverloads fun Widget.tick(isVisible: Boolean=false): ExplorationAction{
+	if(!(visible||isVisible) || !enabled)
 		throw RuntimeException("ERROR: tried to tick non-actable (checkbox) Widget $this")
 	widgetTargets.add(this)
 	return clickCoordinate().let{ (x,y) -> Click(x, y, true) }
 }
 
-fun Widget.longClick(delay:Long = 0): ExplorationAction{
-	if(!visible || !enabled || !longClickable)
+@JvmOverloads fun Widget.longClick(delay:Long = 0, isVisible: Boolean=false): ExplorationAction{
+	if(!(visible||isVisible) || !enabled || !longClickable)
 		throw RuntimeException("ERROR: tried to long-click non-actable Widget $this")
 	widgetTargets.add(this)
 	return clickCoordinate().let{ (x,y) -> LongClick(x, y,true,delay) }
 }
-fun Widget.setText(newContent: String): ExplorationAction{
+@JvmOverloads fun Widget.setText(newContent: String, isVisible: Boolean=false): ExplorationAction{
+	if(!(visible||isVisible) || !enabled || !isEdit)
+		throw RuntimeException("ERROR: tried to enter text on non-actable Widget $this")
 	widgetTargets.add(this)
 	return  TextInsert(this.idHash,newContent,true)
 }
@@ -49,7 +51,11 @@ fun Widget.swipeLeft(stepSize: Int):ExplorationAction = TODO()
 fun Widget.swipeRight(stepSize: Int):ExplorationAction = TODO()
 
 /** navigate to this widget (which may be currently out of screen) and click it */
-fun Widget.navigateTo():ExplorationAction = TODO()
+fun Widget.navigateTo(action:(Widget)->ExplorationAction):ExplorationAction{
+	if (visible) return action(this)
+
+TODO()
+}
 
 /**
  * Used by RobustDevice which does not currently parse Widgets.
