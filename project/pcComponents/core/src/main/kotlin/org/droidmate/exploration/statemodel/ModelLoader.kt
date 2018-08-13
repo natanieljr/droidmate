c2f7815cad598f3ea7c0aeef6a1a99173aa9280e
J.H <hotzkow@cs.uni-saarland.de>
2018-08-13 14:11:59 +0200

    fixed co-routine names in stacktrace of ModelLoader

--- project/pcComponents/core/src/main/kotlin/org/droidmate/exploration/statemodel/ModelLoader.kt
+++ project/pcComponents/core/src/main/kotlin/org/droidmate/exploration/statemodel/ModelLoader.kt
@@ -54,7 +54,9 @@ open class ModelLoader(protected val config: ModelConfig, private val customWidg
 	private val model = Model.emptyModel(config)
 
 	private val job = Job()
-	private val context: CoroutineContext = newCoroutineContext(context = CoroutineName("ModelParsing"), parent = job)
+	private val context: CoroutineContext = newCoroutineContext(context = CoroutineName("ModelParsing ${config.appName}(${config.baseDir})"), parent = job)
+
+	private fun context(name:String, parent:Job = job) = newCoroutineContext(context = CoroutineName(name), parent = parent)
 
 	@Suppress("UNUSED_PARAMETER")
 	private fun log(msg: String) {}//= println("[${Thread.currentThread().name}] $msg")
@@ -108,32 +110,34 @@ open class ModelLoader(protected val config: ModelConfig, private val customWidg
 		log("\n getFileContent skip=$skip, path= ${path.toUri()} \n")
 
 		if (!file.exists()) { return null } // otherwise this state has no widgets
-		file.bufferedReader().use { return it.lines().skip(skip).toList().also {
-			if(path.toUri().toString().contains("trace")) {
-				log(" FILE-Content ")
-				it.forEach { log(it) }
-				log(" END FileContent")
-			}
-		} }
+		file.bufferedReader().use { return it.lines().skip(skip).toList()
+//				.also {
+//			if(path.toUri().toString().contains("trace")) {
+//				log(" FILE-Content ")
+//				it.forEach { log(it) }
+//				log(" END FileContent")
+//			}
+//		}
+		}
 	}
 	private inline fun <reified T> P_processLines(path: Path, skip: Long = 1, crossinline lineProcessor: (List<String>) -> Deferred<T>): List<Deferred<T>> {
 		log("call P_processLines for ${path.toUri()}")
 		getFileContent(path,skip)?.let { br ->	// skip the first line (headline)
-			assert(br.count() > 0, { "ERROR on model loading: file ${path.fileName} does not contain any entries" })
+			assert(br.count() > 0) { "ERROR on model loading: file ${path.fileName} does not contain any entries" }
 			return br.map { line -> lineProcessor(line.split(config[sep]).map { it.trim() }) }
 		} ?: return emptyList()
 	}
-	private val stateTask: (ConcreteId)->Deferred<StateData> = { key -> async(CoroutineName("parseState $key"),parent = job){ P_parseState(key)} }
+	private val stateTask: (ConcreteId)->Deferred<StateData> = { key -> async(context("parseState $key")){ P_parseState(key)} }
 
 	/** compute for each line in the trace file the ActionData object and the resulting StateData object */
 	protected val _actionParser: (List<String>) -> Deferred<Pair<ActionData, StateData>> = { entries ->
 		log("parse action $entries")
-		async(CoroutineName("actionParser"), parent = job) {
+		async(context("actionParser ${entries[ActionData.srcStateIdx]}->${entries[ActionData.resStateIdx]}")) {
 		// we createFromString the source state and target widget if there is any
 		val resState = idFromString(entries[ActionData.resStateIdx]).let { resId ->
 			log("parse result: $resId")
 			// parse the result state with the contained widgets and queue them to make them available to other coroutines
-			stateQueue.computeIfAbsent(resId, stateTask).also { launch{ assert(it.await().stateId == resId, {"ERROR result State $it should have id $resId"})} }
+			stateQueue.computeIfAbsent(resId, stateTask).also { launch{ assert(it.await().stateId == resId) {"ERROR result State $it should have id $resId"} } }
 		}
 		val targetWidget = entries[ActionData.widgetIdx].let { widgetIdString ->
 			if (widgetIdString == "null") null
@@ -143,9 +147,9 @@ open class ModelLoader(protected val config: ModelConfig, private val customWidg
 					stateQueue.computeIfAbsent(srcId, stateTask)
 							.await().let { srcState ->
 								log("SRC-State $srcId computed")
-								assert(srcState.stateId == srcId, {" ERROR source state $srcState should have id $srcId"})
+								assert(srcState.stateId == srcId) {" ERROR source state $srcState should have id $srcId"}
 								srcState.widgets.find { it.id == targetWidgetId }
-										.also { assert(it != null, {" ERROR could not find target widget $targetWidgetId in source state $srcState" }) }
+										.also { assert(it != null) {" ERROR could not find target widget $targetWidgetId in source state $srcState" } }
 							}
 				}
 			}
@@ -165,16 +169,14 @@ open class ModelLoader(protected val config: ModelConfig, private val customWidg
 		val(contentPath,isHomeScreen,topPackage) = getStateFile(stateId)
 		return mutableSetOf<Widget>().apply {	// create the set of contained elements (widgets)
 			log(" parse file ${contentPath.toUri()}")
-				val widgets = P_processLines(path = contentPath, lineProcessor = _widgetParser).map{it.await()}
+			val widgets = P_processLines(path = contentPath, lineProcessor = _widgetParser).map{it.await()}
 
-					widgets.forEach {
-					log("await for each")
-					it.also {
-						// add the parsed widget to temporary set AND initialize the parent property
-						log(" add widget $it")
-						add(it)
-					}
-				}
+			widgets.forEach {
+				log("await for each")
+				// add the parsed widget to temporary set AND initialize the parent property
+				log(" add widget $it")
+				add(it)
+			}
 		}.let {
 			if (it.isNotEmpty())
 				StateData.fromFile(it,isHomeScreen,topPackage).also { newState -> model.addState(newState) }
@@ -194,7 +196,7 @@ open class ModelLoader(protected val config: ModelConfig, private val customWidg
 		Pair((UUID.fromString(line[Widget.idIdx.first])), wConfigId).let { widgetId ->
 			widgetQueue.computeIfAbsent(widgetId) { id ->
 				log("parse widget absent $id")
-				async(CoroutineName("parseWidget $id"), parent = job) {
+				async(context("parseWidget $id")) {
 					Widget.fromString(line,customWidgetIndicies).also { widget ->
 						model.S_addWidget(widget)  // add the widget to the model if it didn't exist yet
 						assert(id == widget.id)	{ "ERROR on widget parsing inconsistent ID created ${widget.id} instead of $id" }
