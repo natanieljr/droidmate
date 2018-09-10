68de1beae1b03f2756142abbb9627ed678232339
natanieljr <nataniel.borges@cispa.saarland>
2018-09-10 11:07:27 +0200

    Compatibilized with DM2-1.1

--- droidmate_usage_examples/src/main/kotlin/org/droidmate/example/ExampleStrategy.kt
+++ droidmate_usage_examples/src/main/kotlin/org/droidmate/example/ExampleStrategy.kt
@@ -1,20 +1,25 @@
 package org.droidmate.example
 
-import org.droidmate.exploration.actions.AbstractExplorationAction
-import org.droidmate.exploration.actions.PressBackExplorationAction
-import org.droidmate.exploration.actions.ResetAppExplorationAction
-import org.droidmate.exploration.actions.TerminateExplorationAction
+import org.droidmate.deviceInterface.guimodel.ExplorationAction
+import org.droidmate.exploration.actions.click
+import org.droidmate.exploration.actions.pressBack
+import org.droidmate.exploration.actions.resetApp
+import org.droidmate.exploration.actions.terminateApp
 import org.droidmate.exploration.strategy.AbstractStrategy
 
 class ExampleStrategy(private val someId: Int): AbstractStrategy(){
 	// Model features can be accessed from the strategy as well
 	val modelFeature : ExampleModelFeature by lazy { eContext.getOrCreateWatcher<ExampleModelFeature>() }
 
-	override fun internalDecide(): AbstractExplorationAction {
+	override fun internalDecide(): ExplorationAction {
 		return when {
-			eContext.isEmpty() -> ResetAppExplorationAction()
-			modelFeature.count == someId -> TerminateExplorationAction()
-			else -> return PressBackExplorationAction()
+			eContext.isEmpty() -> eContext.resetApp()
+
+			modelFeature.count == someId -> terminateApp()
+
+			currentState.actionableWidgets.isNotEmpty() -> currentState.actionableWidgets.first().click()
+
+			else -> eContext.pressBack()
 		}
 	}
 
