a277e03336ed30e2c0badfc80b1f24937c9e7f30
Timo Gühring <timo.guehring@googlemail.com>
2019-03-22 13:35:38 +0100

    Add appextractor.py for downloading apps from the Google play store. Remove ancient scripts.

--- project/scripts/get_current_activity_name.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash          
-
-# Based on: http://stackoverflow.com/questions/13193592/adb-android-getting-the-name-of-the-current-activity
-adb shell dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp'
-
